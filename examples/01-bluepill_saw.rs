37aa5f62a64f9eaf94d0f2e37141be9649a58624
Mendelt Siebenga <msiebenga@gmail.com>
2019-05-05 21:34:10 +0200

    Add bluepill example (mostly for debugging and dev)

---
+++ examples/01-bluepill_saw.rs
@@ -0,0 +1,43 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use stm32f1xx_hal::prelude::*;
+use stm32f1xx_hal::pac;
+use stm32f1xx_hal::i2c::{Mode, BlockingI2c};
+use embedded_hal::blocking::i2c::Write;
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Configure the clock
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Configure the pins for I2C1
+    let mut scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let mut sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    // Configure I2C
+
+    let mut i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Standard{frequency: 10000},
+        clocks,
+        &mut rcc.apb1,
+        100,100,100,100,
+    );
+
+    loop {
+        i2c.write(7, &[3]);
+    }
+}
