703b96357385049e63d56a91b0feec402deb5731
Mendelt Siebenga <msiebenga@gmail.com>
2019-05-08 22:44:29 +0200

    Add first "working" example

--- examples/01-bluepill_saw.rs
+++ examples/01-bluepill_saw.rs
@@ -1,12 +1,15 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_semihosting;
-
 use cortex_m_rt::entry;
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::pac;
 use stm32f1xx_hal::i2c::{Mode, BlockingI2c};
+use stm32f1xx_hal::delay::Delay;
+
+#[allow(unused_imports)]
+use panic_semihosting;
+use mcp4725::*;
 
 #[entry]
 fn main() -> ! {
@@ -24,6 +27,8 @@ fn main() -> ! {
     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
     let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
 
+    // let mut delay = Delay::new(cp.SYST, clocks);
+
     // Configure I2C
 
     let mut i2c = BlockingI2c::i2c1(
@@ -33,10 +38,19 @@ fn main() -> ! {
         Mode::Standard{frequency: 10000},
         clocks,
         &mut rcc.apb1,
-        100,100,100,100,
+        1000,10,1000,1000,
     );
 
+    let mut dac = MCP4725::create(i2c);
+    let mut dac_cmd = Command::default();
+
+    dac_cmd = dac_cmd.address(0b00000010);
+    let mut value: u16 = 0;
+
     loop {
-        i2c.write(7, &[3]);
+        dac_cmd = dac_cmd.data(value);
+        dac.send(&dac_cmd);
+        value += 1;
+        value &= 0x0fff;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,93 @@
+#![no_std]
+
+//use nb::Result;
+use embedded_hal::blocking::i2c::Write;
+use core::cmp;
+
+pub struct MCP4725<I2C> where I2C: Write {
+    i2c: I2C
+}
+
+impl<I2C> MCP4725<I2C> where I2C: Write {
+    pub fn create(i2c: I2C) -> Self {
+        MCP4725 { i2c }
+    }
+
+    pub fn send(&mut self, command: &Command) {
+        self.i2c.write(command.address_byte(), &command.data_bytes());
+    }
+}
+
+const DEVICE_ID: u8 = 0b1100;
+
+pub struct Command {
+    address: u8,
+    write_eeprom: bool,
+    power_mode: PowerMode,
+    data: u16,
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self {
+            address: 0,
+            write_eeprom: false,
+            power_mode: PowerMode::Normal,
+            data: 0,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(u8)]
+pub enum PowerMode {
+    Normal=0b00,
+    Resistor1kOhm=0b01,
+    Resistor100kOhm=0b10,
+    Resistor500kOhm=0b11,
+}
+
+impl Command {
+    pub fn address_byte(&self) -> u8 {
+        self.address
+    }
+
+    /// Format data bytes to send to the DAC. At the moment only sending one sample at a time is
+    /// supported.
+    pub fn data_bytes(&self) -> [u8;3] {
+        [
+            if self.write_eeprom == true {0x60} else {0x20} + (self.power_mode as u8) << 1,
+            (self.data >> 4) as u8,
+            (self.data & 0x000f << 4) as u8
+        ]
+    }
+
+    /// Set the data to send with this command. This data will be truncated to a 12 bit int
+    pub fn data(mut self, data: u16) -> Self {
+        self.data = cmp::min(data, 0x0fff);
+        self
+    }
+
+    /// Set the 3 bit address to use with this command
+    pub fn address(mut self, address: u8) -> Self {
+        self.address = (DEVICE_ID << 3) + (address & 0b00000111);
+        self
+    }
+
+    /// Write the supplied values to the EEPROM as well as to the DAC
+    pub fn write_eeprom(mut self, write: bool) -> Self {
+        self.write_eeprom = write;
+        self
+    }
+
+    /// Set the power mode
+    pub fn power_mode(mut self, mode: PowerMode) -> Self {
+        self.power_mode = mode;
+        self
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
