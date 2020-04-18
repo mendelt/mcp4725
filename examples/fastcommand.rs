f91ee5c3e0e589ae28aa027a0a5c3b9686867dcb
Mendelt Siebenga <msiebenga@gmail.com>
2020-04-18 14:12:35 +0200

    add relevant examples from the bluepill-examples project here

---
+++ examples/fastcommand.rs
@@ -0,0 +1,60 @@
+//! Example that creates a square wave (alternating high and low) using the MCP4725 driver sending
+//! fast-commands. This example is written and tested on the STM32f103 on the bluepill board.
+
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use hal::i2c::{BlockingI2c, Mode};
+use hal::pac;
+use hal::prelude::*;
+use hal::time::U32Ext;
+
+use mcp4725::*;
+#[allow(unused_imports)]
+use panic_semihosting;
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Configure the clock
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Configure the pins for I2C1
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    // Configure I2C
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Standard {
+            frequency: 400000.hz(),
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    // Configure the MCP4725 DAC
+    let mut dac = MCP4725::create(i2c);
+
+    // Slowly decrease the output of the DAC to its minimum value, then start over
+    let cmd_high = FastCommand::default().address(0b010).data(0x0fff);
+    let cmd_low = FastCommand::default().address(0b010).data(0x0000);
+
+    loop {
+        dac.send_fast(&cmd_high);
+        dac.send_fast(&cmd_low);
+    }
+}
---
+++ examples/saw.rs
@@ -0,0 +1,62 @@
+//! MPC4725 for the stm32f103. It uses the I2C 1 interface on pins pb8 and pb9 to control the DAC
+
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use hal::i2c::{BlockingI2c, Mode};
+use hal::pac;
+use hal::prelude::*;
+use hal::time::U32Ext;
+
+use mcp4725::*;
+#[allow(unused_imports)]
+use panic_semihosting;
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Configure the clock
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Configure the pins for I2C1
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    // Configure I2C
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Standard {
+            frequency: 400000.hz(),
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    // Configure the MCP4725 DAC
+    let mut dac = MCP4725::create(i2c);
+
+    // Create the dac command and set it to the right address
+    let mut dac_cmd = Command::default().address(0b010);
+
+    // Slowly increase the output of the DAC to it's maximum value, then start over
+    let mut value: u16 = 0;
+    loop {
+        dac_cmd = dac_cmd.data(value);
+        dac.send(&dac_cmd);
+        value += 1;
+        value &= 0x0fff;
+    }
+}
